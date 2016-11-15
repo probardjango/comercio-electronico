742d285fad49e09bec5a6fa0f830b329f6c29d4e
Karlita K <Karlita K>
2016-11-15 21:44:53 +0100

    remove all pyc files + LIST VIEW

--- src/productos/views.py
+++ src/productos/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 from .models import Producto
 # Create your views here.
+
+class ProductoListView(ListView):
+	model = Producto
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(ProductoListView, self).get_context_data(*args, **kwargs)
+		context["ahora"] = timezone.now()
+		return context
 
 class ProductoDetailView(DetailView):
 	model = Producto
