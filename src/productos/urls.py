e643272a6876954e18d96af39f7dac81c6f906ba
Karlita K <Karlita K>
2016-11-15 20:10:23 +0100

    all pyc files removed

--- src/productos/urls.py
+++ src/productos/urls.py
@@ -3,10 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from .views import ProductoDetailView
+from .views import ProductoDetailView, ProductoListView
 # from boletin.views import inicio
 
 urlpatterns = [
     #url(r'^$', views.inicio, name='inicio'),
-    url(r'^(?P<pk>\d+)/$', ProductoDetailView.as_view(), name='producto_detail')
+    url(r'^(?P<pk>\d+)/$', ProductoDetailView.as_view(), name='producto_detail'),
+    url(r'^$', ProductoListView.as_view(), name='producto_list'),
 ]
+
