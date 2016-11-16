7a66a5a388cf68febcc25af18066596c0d1ae4be
Karlita K <Karlita K>
2016-11-16 10:53:26 +0100

    Model Manager .activo()

--- src/productos/models.py
+++ src/productos/models.py
@@ -4,13 +4,27 @@ from django.core.urlresolvers import reverse
 from django.db import models
 
 # Create your models here.
+class ProductoQuerySet(models.query.QuerySet):
+	def activo(self):
+		return self.filter(activo=True)
+
+class ProductoManager(models.Manager):
+	def get_queryset(self):
+		return ProductoQuerySet(self.model, using=self._db) 
+
+	def all(self, *args, **kwargs):
+		return self.get_queryset().activo()
+
+
 class Producto(models.Model):
 	titulo = models.CharField(max_length=120)
 	descripcion = models.TextField(blank=True, null=True)
 	precio = models.DecimalField(decimal_places=2, max_digits=10)
 	activo = models.BooleanField(default=True)
 	#slug
-	#stock 
+	#stock
+
+	objects = ProductoManager()
 
 	def __unicode__(self): #2
 		return self.titulo 
