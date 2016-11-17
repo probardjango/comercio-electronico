bf8f1b88bbda59e5c0a487ae0f8d834cf0f2bec6
Karlita K <Karlita K>
2016-11-17 14:29:29 +0100

    busquedas complejas con Q

--- src/productos/views.py
+++ src/productos/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.utils import timezone
 from django.views.generic.detail import DetailView
@@ -11,8 +12,18 @@ class ProductoListView(ListView):
 
 	def get_context_data(self, *args, **kwargs):
 		context = super(ProductoListView, self).get_context_data(*args, **kwargs)
-		context["ahora"] = timezone.now()
+		context["query"] = self.request.GET.get("q")
 		return context 
+
+	def get_queryset(self, *args, **kwargs):
+		qs = super(ProductoListView, self).get_queryset(*args, **kwargs)
+		query = self.request.GET.get("q")
+		if query:
+			qs = self.model.objects.filter(
+				Q(titulo__icontains=query) |
+				Q(descripcion__icontains=query)
+			)
+		return qs
 
 class ProductoDetailView(DetailView):
 	model = Producto
