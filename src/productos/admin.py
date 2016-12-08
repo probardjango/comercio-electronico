738daa9acf7a58d6e42a5859395b041d9b9602ce
Karlita K <Karlita K>
2016-12-08 12:42:33 +0100

    Admin Inlines para Modificar Productos

--- src/productos/admin.py
+++ src/productos/admin.py
@@ -4,9 +4,26 @@ from django.contrib import admin
 from .models import Producto, Caracteristica, ProductoImg, Categoria, ProductoDestacado
 # Register your models here.
 
+class ProductoImgInline(admin.TabularInline):
+	model = ProductoImg
+	extra = 0
 
-admin.site.register(Producto)
-admin.site.register(Caracteristica)
-admin.site.register(ProductoImg)
+class CaracteristicaInline(admin.TabularInline):
+	model = Caracteristica
+	extra = 0
+
+class ProductoAdmin(admin.ModelAdmin):
+	list_display = ['__unicode__', 'precio']
+	inlines = [
+		CaracteristicaInline,
+		ProductoImgInline,
+	]
+	class Meta:
+		model = Producto
+
+
+admin.site.register(Producto, ProductoAdmin)
+# admin.site.register(Caracteristica)
+# admin.site.register(ProductoImg)
 admin.site.register(Categoria)
 admin.site.register(ProductoDestacado)
